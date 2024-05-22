77dd6bf04813face01dfbe64cbb7512a5741c911
KoliaKosyanenko <112539586+KoliaKosyanenko@users.noreply.github.com>
2024-05-22 21:31:42 +0300

    add new btn

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,9 +1,14 @@
 import axios from 'axios';
 
-const apiKey = '43687048-bb18a09d847445a540140347b';
+let curentPage = 1;
 
+const apiKey = '43687048-bb18a09d847445a540140347b';
+const options = new URLSearchParams({
+  page: curentPage,
+  per_page: 9,
+});
 export async function searchImages(query) {
-  const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`;
+  const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&${options}`;
 
   try {
     const response = await axios.get(apiUrl);
